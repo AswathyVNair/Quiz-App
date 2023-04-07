08bd319d2a0d5687d949cbaa21eaf9b3c10f7aee
AswathyVNair <aswathy.code@gmail.com>
2023-04-07 10:55:12 +0530

    initial commit
    creted login.js

---
+++ quiz_db.sql
@@ -0,0 +1,17 @@
+/* Creating database*/
+CREATE DATABASE quiz_app;
+
+-- creating table users
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Inserting dummy values
+INSERT INTO users (username, email, password)
+VALUES ('john123', 'john@example.com', 'password123');
+
+--showing table
